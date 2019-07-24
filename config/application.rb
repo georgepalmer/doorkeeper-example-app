024a304df81eff6852af3a19504b4150bf8f13a9
George Palmer <george.palmer@gmail.com>
2019-07-24 18:01:27 +0100

    Throw exception on bad params

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module DoorkeeperExample
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    config.action_controller.action_on_unpermitted_parameters = :raise
   end
 end
